406112e787e955f4c813188446d7198a02dc11ae
Guilherme Gwadera <16023489+ggwadera@users.noreply.github.com>
2020-08-12 00:08:01 -0300

    remove unused routes

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
-import Index from './pages/Index';
 import PokemonLanding from './pages/PokemonLanding';
 
 function Routes() {
   return (
     <BrowserRouter>
-      <Route path="/" exact component={Index} />
-      <Route path="/addLesson" component={PokemonLanding} />
+      <Route path="/" exact component={PokemonLanding} />
     </BrowserRouter>
   )
 }
