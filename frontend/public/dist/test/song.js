96407c237f2bd1bff2a8c0428562f3353e22f456
sernie zanders <bernie@zheng.network>
2020-08-16 09:21:22 -0700

    clean up code to only have bare minimum

--- frontend/public/dist/test/song.js
+++ frontend/public/dist/test/song.js
@@ -1,51 +1,31 @@
-const allChunks = [];
 const container = document.querySelector(".container");
 let count = 0;
 
-const mediaSource = new MediaSource();
-let sourceBuffer;
-mediaSource.addEventListener("sourceopen", function () {
-  sourceBuffer = mediaSource.addSourceBuffer('audio/webm;codecs="opus"');
-  //sourceBuffer = mediaSource.addSourceBuffer('video/webm; codecs="opus, vp9"');
-  console.log("sourceopen", sourceBuffer);
-});
+function AudioStream() {
+  const mediaSource = new MediaSource();
+  let sourceBuffer;
+  mediaSource.addEventListener("sourceopen", function () {
+    sourceBuffer = mediaSource.addSourceBuffer('audio/webm;codecs="opus"');
+    //sourceBuffer = mediaSource.addSourceBuffer('video/webm; codecs="opus, vp9"');
+    console.log("sourceopen", sourceBuffer);
+  });
 
-const addBuffer = async (buffer) => {
-  const reader = new FileReader();
-  reader.readAsArrayBuffer(buffer);
-  reader.onload = () => {
-    console.log(reader.result, sourceBuffer);
-    console.log("before append", sourceBuffer.updating);
-    //sourceBuffer.appendBuffer(new Uint8Array(buffer));
-    sourceBuffer.appendBuffer(reader.result);
-    console.log("after append", sourceBuffer.updating);
+  const mediaSourceUrl = URL.createObjectURL(mediaSource);
+  const myVideo = document.querySelector("#myVideo");
+  //myVideo.src = mediaSourceUrl;
+  const myAudio = new Audio(mediaSourceUrl);
+
+  this.addBuffer = (buffer) => {
+    sourceBuffer.appendBuffer(buffer);
   };
-};
-
-const mediaSourceUrl = URL.createObjectURL(mediaSource);
-const myVideo = document.querySelector("#myVideo");
-//myVideo.src = mediaSourceUrl;
-const myAudio = new Audio(mediaSourceUrl);
+  this.play = () => {
+    myAudio.play();
+  };
+}
 
 //window.song = myAudio;
 
-function Block(chunk, index) {
-  const div = document.createElement("div");
-  div.className = "block";
-  div.onclick = () => {
-    const audioUrl = URL.createObjectURL(chunk);
-    const audio = new Audio(audioUrl);
-    audio.play();
-
-    const reader = new FileReader();
-    reader.readAsDataURL(chunk);
-    reader.onload = () => {
-      console.log(reader.result);
-    };
-  };
-  div.innerText = index;
-  container.append(div);
-}
+const anotherAudio = new AudioStream();
 
 const recordAudio = async () => {
   const stream = await navigator.mediaDevices.getUserMedia({
@@ -56,23 +36,24 @@ const recordAudio = async () => {
     mimeType: 'audio/webm; codecs="opus"',
   });
 
+  const addBuffer = async (blob) => {
+    // Since mediaSource only takes in array buffer and we have audio blob
+    //   we need to convert audio blob into array buffer with FileReader
+    const reader = new FileReader();
+    reader.readAsArrayBuffer(blob);
+    reader.onload = () => {
+      anotherAudio.addBuffer(reader.result);
+    };
+  };
+
   mediaRecorder.addEventListener("dataavailable", (event) => {
-    allChunks.push(event.data);
     addBuffer(event.data);
-    new Block(event.data, count);
-    count += 1;
+    if (!count) {
+      anotherAudio.play();
+    }
   });
 
-  mediaRecorder.start(2000);
-};
-
-const play = (idx) => {
-  const audioBlob = new Blob([allChunks[0], allChunks[idx]]);
-  const audioUrl = URL.createObjectURL(audioBlob);
-  const audio = new Audio(audioUrl);
-  audio.play();
+  mediaRecorder.start(50);
 };
 
 recordAudio();
-
-setTimeout(() => {}, 5000);
